78d7407591bc83234a7b4e1a0c770a77dc3e027d
Arpit Bansal <arpitbansal761943@gmail.com>
2024-06-04 16:38:01 +0530

    writing api in fastapi, llm code and fucntions, dockerfile

--- cookbook/llm.py
+++ cookbook/llm.py
@@ -1,12 +1,9 @@
-from ..schemas import ChatRequest
 
-from langchain.llms import ctransformers
-from langchain.memory import ConversationBufferMemory #, ConversationSummaryMemory, ConversationSummaryBufferMemory
-from langchain.chains.llm import LLMChain
-from ctransformers import AutoModelForCausalLM
-from fastapi import FastAPI
-from llama_guard import guard, parser, build_prompt, check
+# from langchain.memory import ConversationBufferWindowMemory, ConversationBufferMemory
+# from langchain.chains import LLMChain, ConversationChain#, conversational_retrieval
+# from fastapi import FastAPI
 
+from llama_cpp import Llama
 
 system_prompt = """You are a excellent counsellor that helps learner with their mental health, their obstacles in education and their day-to-day life problems
                 user will ask you questions and you will carefully answer them"""
@@ -15,24 +12,52 @@ B_SYS, E_SYS = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>", "<
 ASSISTANT_INST = "<|start_header_id|>assistant<|end_header_id|>"
 SYSTEM_PROMPT = B_SYS + system_prompt + E_SYS
 
+def prompt_for_chat(content:str):
+  return [{"role": "system", "content": """You are an excellent counselor who assists user with their mental health,
+            educational challenges, and everyday life issues.
+            user will ask you questions, and you will provide thoughtful answers."""},
+          {"role": "user",
+           "content":content}]
 
-llm = AutoModelForCausalLM.from_pretrained(model_path_or_repo_id="hf_repo_id", model_type='llama')
-memory  = ConversationBufferMemory(input_key="question", memory_key="chat_history")
+model = Llama.from_pretrained(repo_id="Arpit-Bansal/counsellor_model_q5_k_m",
+                              filename="counsellor_model_q5_k_m-unsloth.Q5_K_M.gguf",
+                              verbose=False)
 
-def llm_function(user_input):
-    memory.chat_memory.add_user_message(user_input)
-    chat_history = memory.load_memory_variables({})["chat_history"]
-    prompt = f"{SYSTEM_PROMPT}\n\n{chat_history}\n{B_INST} {user_input} {E_INST}"
-    llm_response = llm(prompt=prompt, temperature=0.7, repetition_penalty=1.2, threads=3, max_new_tokens=2048)
-    memory.chat_memory.add_ai_message(llm_response)
+def prompt_for_chat(content:str):
+  return [{"role": "system", "content": """You are an excellent counselor who assists user with their mental health,
+            educational challenges, and everyday life issues.
+            and you will provide thoughtful answers.""",
+          },
+          { "role": "user",
+           "content":f"{content}{E_INST}"}]
 
-    return llm_response
-    
+def response_return(response):
+  res = ""
+  for chunk in response:
+    delta = chunk["choices"][0]["delta"]
+    if "content" not in delta:
+        continue
+    res += delta["content"]
+  return res
+
+# memory= ConversationBufferWindowMemory(input_key="question", memory_key="chat_history", ai_prefix="assistant", human_prefix="user", k=4)
+
+def llm_function(user_input:str):
+    # chat_history = memory.load_memory_variables({})["chat_history"]
+    # prompt = f"{SYSTEM_PROMPT}\n\n{chat_history}\n{B_INST} {user_input} {E_INST}"
+    print(prompt_for_chat(content=user_input))
+    llm_response = model.create_chat_completion(messages=prompt_for_chat(content=user_input),
+                                        stream = True, temperature = 0.6, max_tokens = 256)
+    resp = response_return(llm_response)
+    # memory.chat_memory.add_user_message(user_input)
+    # memory.chat_memory.add_ai_message(resp)
+    return resp
 #llama-guard-2
 
-app = FastAPI()
+# app = FastAPI()
 
 
-@app.post("/")
-async def stream(item:ChatRequest):
-    return llm_function(item)
+
+# @app.post("/")
+# async def stream(item:ChatRequest):
+#     return llm_function(item)
--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-from flask import Flask, request, jsonify
+# from flask import Flask, request, jsonify
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, this is the AI chat API for Ollama."
+# app = Flask(__name__)
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    data = request.json
-    user_message = data.get('message')
-    # integrate your AI model to generate a response
-    ai_response = generate_response(user_message)
-    return jsonify({"response": ai_response})
+# @app.route('/')
+# def index():
+#     return "Hello, this is the AI chat API for Ollama."
 
-def generate_response(user_message):
-    # Placeholder function for AI model integration
-    return f"Echo: {user_message}"
+# @app.route('/chat', methods=['POST'])
+# def chat():
+#     data = request.json
+#     user_message = data.get('message')
+#     # integrate your AI model to generate a response
+#     ai_response = generate_response(user_message)
+#     return jsonify({"response": ai_response})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# def generate_response(user_message):
+#     # Placeholder function for AI model integration
+#     return f"Echo: {user_message}"
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# fastapi_implementation
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from schemas import ChatRequest
+from cookbook.llm import llm_function
+from cookbook.llama_guard import check
+
+app=FastAPI()
+
+@app.post("/counsellor")
+def counsellor(quest:str):
+    if check(role="user", content=quest) == 'safe':
+        respon = llm_function(user_input=quest)
+        return respon
+    else:
+        return "invalid_request"
